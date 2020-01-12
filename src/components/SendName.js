011fa8202243e57411db073c64180313c0ecc13b
sebaspa <sebaspa0107@gmail.com>
2020-01-11 22:54:55 -0500

    Create send name component

---
+++ src/components/SendName.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const SendName = () => (
+    <div className="send-name">
+        <div className="container">
+            <div className="row">
+                <div className="col s12">
+                    <div className="cont-send-name">
+                        <div className="col s12 l8 xl9">
+                            <div className="cont-txt">
+                                <p className="grey-text text-darken-4 light-text">Is there anyone else you would want us to add?</p>
+                            </div>
+                        </div>
+                        <div className="col s12 l4 xl3">
+                            <div className="cont-btn">
+                                <a href="/#" className="btn grey-text text-darken-4 text-center">Submit a Name</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+);
+
+export default SendName;
